1aa8a10238f3c02366333d5ea4bb503a7510bb4e
susan.ludwigsen <susanludwigsen@gmail.com>
2015-01-21 19:30:17 -0800

    complete assignment2
    Replace stubs with functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the user to perform matrix inversion using a cache 
+## to improve efficiency
 
-## Write a short comment describing this function
+## create a vector of functions to do cached matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve(x)
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Invert a matrix using the makeCacheMatrix function to improve efficiency
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
